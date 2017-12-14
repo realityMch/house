06397ab3bf3a1e986a3699f6cd5ff5dbf9853a2e
mengchao <344580221@qq.com>
2017-12-14 17:59:22 +0800

    spring-data-redis

---
+++ book/src/main/java/com/carlmung/rest/book/redis/RedisSessionConfig.java
@@ -0,0 +1,10 @@
+package com.carlmung.rest.book.redis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+//maxInactiveIntervalInSeconds session超时时间,单位秒
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
+public class RedisSessionConfig {
+}
